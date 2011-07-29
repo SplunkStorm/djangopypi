2a2d545a61863b152d8f17b297041ee344947e1e
Alex Holmes <alex@alex-holmes.com>
2011-07-29 14:45:48 +0100

    First draft of an distribution import django management script
    Using the pkginfo package, we can introspect distribution files (assuming
    they're on the local machine), meaning that a django management script can
    pop the packages into our new dists directory and then put the metadata
    straight into the database via django's ORM, bypassing all of the upload
    mechanism.

---
+++ djangopypi/management/commands/import_packages.py
@@ -0,0 +1,148 @@
+from djangopypi.models import *
+from djangopypi import conf
+from django.contrib.auth.models import Group
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from pkginfo import BDist, SDist
+
+from optparse import make_option
+import sys, os, shutil
+
+class Command(BaseCommand):
+
+    DEFAULT_OWNERS = [
+        Group.objects.get_or_create(name='somegroup')[0]
+    ]
+    DEFAULT_DPS = [
+        Group.objects.get(name='somegroup')
+    ]
+    DEFAULT_UPLOADER = User.objects.get(username='someuser')
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+
+    def handle(self, *args, **kwargs):
+        for filename in args:
+            self._curfile = filename
+            try:
+                if filename.endswith('.zip') or filename.endswith('.tar.gz') or \
+                                                        filename.endswith('.tgz'):
+                    package = SDist(self._curfile)
+                    self._log(filename, package, *self._add_dist(package))
+                elif filename.endswith('.egg'):
+                    package = BDist(self._curfile)
+                    self._log(filename, package, *self._add_dist(package))
+
+            except ValueError, e:
+                print >>sys.stderr, 'Importing: %s: ERROR\n\t%s' % (filename, e.message)
+
+    def _log(self, filename, package, pkg_created, release_created, dist_created):
+        """ Log logic """
+        if dist_created:
+            result = 'Success'
+        else:
+            result = 'FAILED'
+
+        print >>sys.stderr, 'Importing %s: %s. [Created Package(): %r. Created Release(): %r.]' % (
+                                filename, result, pkg_created, release_created)
+
+    def _add_dist(self, dist_data):
+        package, created_package  = Package.objects.get_or_create(
+            name=dist_data.name,
+        )
+
+        if created_package:
+            for owner in self.DEFAULT_OWNERS:
+                package.owners.add(owner)
+            for download_perm in self.DEFAULT_DPS:
+                package.download_permissions.add(download_perm)
+
+        package_info_field = self._package_info(dist_data)
+
+        release, created_release = Release.objects.get_or_create(
+            package=package,
+            version=dist_data.version,
+            metadata_version=dist_data.metadata_version,
+            package_info=self._package_info(dist_data)
+        )
+
+        dist_file = self._copy_dist_file(dist_data)
+        if dist_file:
+            distribution, created_dist = Distribution.objects.get_or_create(
+                release = release,
+                content=dist_file,
+                md5_digest=self._md5_digest(dist_data),
+                filetype=self._get_filetype(dist_data),
+                pyversion=self._get_pyversion(dist_data),
+                uploader=self.DEFAULT_UPLOADER,
+            )
+        else:
+            created_dist = False
+
+        return created_package, created_release, created_dist
+
+    def _package_info(self, dist_data):
+        fields = list(conf.METADATA_FIELDS[dist_data.metadata_version])
+
+        # pkginfo gets PEP241 wrong, calling platform 'platforms'.
+        if 'platform' in fields:
+            fields.remove('platform')
+            fields.append('platforms')
+
+        info = []
+        for f in fields:
+            try:
+                info.append((f, getattr(dist_data, f)))
+            except AttributeError:
+                print >>sys.stderr, 'Could not find %s in the dist data.' % f
+
+        return dict(info)
+
+    def _copy_dist_file(self, dist_data):
+        '''Move the file to the media folder, then return the new location'''
+        upload_directory = os.path.join(
+            settings.MEDIA_ROOT,
+            conf.RELEASE_UPLOAD_TO
+        )
+
+        try:
+            new_path = os.path.join(upload_directory, os.path.basename(self._curfile))
+            if os.path.exists(new_path):
+                shutil.copy(self._curfile, upload_directory)
+            else:
+                print >>sys.stderr, 'File already exists: %s' % new_path
+            return new_path
+        except IOError:
+            print >>sys.stderr, 'Could not copy file to upload directory'
+            return None
+
+    def _md5_digest(self, dist_data):
+        import md5
+
+        BLOCKSIZE = 1024*1024
+
+        def hexify(s):
+            return ("%02x"*len(s)) % tuple(map(ord, s))
+
+        f = open(self._curfile, "rb")
+        sum = md5.new()
+        while 1:
+            block = f.read(BLOCKSIZE)
+            if not block:
+                break
+            sum.update(block)
+        f.close()
+        return hexify(sum.digest())
+
+    def _get_pyversion(self, dist_data):
+        #TODO: Erm pkginfo can haz pyversion?!
+        return ''
+
+    def _get_filetype(self, dist_data):
+        if isinstance(dist_data, SDist):
+            return 'sdist'
+        elif isinstance(dist_data, BDist):
+            return 'bdist'
+        else:
+            return ''
