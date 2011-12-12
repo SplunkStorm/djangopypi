f78178c5b2942007248e15b32673331edcfd3a6a
Alex Holmes <alex@alex-holmes.com>
2011-12-12 15:40:16 +0000

    Add another batch admin action for package download perms

--- djangopypi/admin.py
+++ djangopypi/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from djangopypi.models import Package, Release, Classifier, \
                               Distribution, Review
 
@@ -18,6 +19,20 @@ def make_anonymous(modeladmin, request, queryset):
     modeladmin.message_user(request, "Made %s packages anonymous" % queryset.count())
 make_anonymous.short_description = "Make packages anonymous"
 
+def available_to_authed_users(modeladmin, request, queryset):
+    for obj in queryset:
+        if isinstance(obj, Package):
+            obj.download_permissions.clear()
+            for group in Group.objects.all():
+                if not group in obj.download_permissions.all():
+                    obj.download_permissions.add(group)
+    modeladmin.message_user(request,
+        "Made %s packages available only to authenticated users." % (
+            queryset.count()
+    ))
+available_to_authed_users.short_description = "Make packages available only " \
+                                              "to authenticated users"
+
 class FullDeletingModelAdmin(admin.ModelAdmin):
     actions = ['full_delete_selected']
     def get_actions(self, request):
@@ -26,7 +41,10 @@ class FullDeletingModelAdmin(admin.ModelAdmin):
         return actions
 
 class PackageModelAdmin(FullDeletingModelAdmin):
-    actions = [make_anonymous]
+    actions = [
+        make_anonymous,
+        available_to_authed_users,
+    ]
     list_display = (
         'name',
     )
